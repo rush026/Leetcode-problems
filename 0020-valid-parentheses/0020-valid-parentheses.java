0bc73c1d82541af3be5bfb648ee01eec315d7e47
RAM PARMESHWAR CHAUDHARY <161919413+rush026@users.noreply.github.com>
2025-09-04 08:32:43 +0530

    Time: 3 ms (41.27%), Space: 41.8 MB (52.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public char getval(char ch){
+        switch(ch){
+            case ']' :return '[';
+            case '}' :return '{';
+            case ')' :return '(';
+            default  : return ' ';
+        }
+       
+    }
+    public boolean isValid(String s) {
+       String opening="[{(";
+       String closing="]})";
+       Stack<Character > st=new Stack<>();
+       for(char ch:s.toCharArray()){
+        if(opening.indexOf(ch)!=-1)
+            st.push(ch);
+        
+        else{
+            if(st.size()==0) return false;
+            char temp=st.pop();
+            if(getval(ch)!=temp)
+            return false;
+        }
+       }
+       return st.size()==0;
+    }
+}
